cd2ab1cea6fe2de5cd4434e8451b5b18d88b4fc8
tubone <tubo.yyyuuu@gmail.com>
2024-03-07 06:26:28 +0900

    fix

--- src/build.ts
+++ src/build.ts
@@ -13,23 +13,25 @@ const options: Options = {
 const data = {
     name: chalk.white("               Yu Otsubo"),
     handle: chalk.white("tubone24"),
-    work: chalk.white("FullCycle Developer"),
+    work: chalk.white("FullCycle Developer") + chalk.cyan("@") + chalk.greenBright("KAG"),
     twitter: chalk.gray("https://twitter.com/") + chalk.cyan("meitante1conan"),
     npm: chalk.gray("https://npmjs.com/") + chalk.red("~tubone24"),
     pypi: chalk.gray("https://pypi.org/user/") + chalk.yellow("tubone24"),
     github: chalk.gray("https://github.com/") + chalk.green("tubone24"),
-    wantedly: chalk.gray("https://www.wantedly.com/id/") + chalk.blueBright("yu_otsubo"),
+    facebook: chalk.gray("https://www.facebook.com/") + chalk.blueBright("yu.otsubo"),
     portfolio: chalk.cyan("https://portfolio.tubone-project24.xyz"),
     blog: chalk.cyan("https://blog.tubone-project24.xyz"),
+    contact: chalk.cyan("https://portfolio.tubone-project24.xyz/#contact"),
     npx: chalk.red("npx") + " " + chalk.white("tubone24"),
     labelWork: chalk.white.bold("       Work:"),
     labelTwitter: chalk.white.bold("    Twitter:"),
     labelnpm: chalk.white.bold("        npm:"),
     labelpypi: chalk.white.bold("       PyPI:"),
     labelGitHub: chalk.white.bold("     GitHub:"),
-    labelWantedly: chalk.white.bold("   Wantedly:"),
+    labelFacebook: chalk.white.bold("   Facebook:"),
     labelPortfolio: chalk.white.bold("  Portfolio:"),
     labelBlog: chalk.white.bold("       Blog:"),
+    labelContact: chalk.white.bold("    Contact:"),
     labelCard: chalk.white.bold("       Card:")
 };
 
@@ -40,21 +42,23 @@ const twittering = `${data.labelTwitter}  ${data.twitter}`;
 const npming = `${data.labelnpm}  ${data.npm}`;
 const pyping = `${data.labelpypi}  ${data.pypi}`;
 const githubing = `${data.labelGitHub}  ${data.github}`;
-const wantedlining = `${data.labelWantedly}  ${data.wantedly}`;
+const facebooking = `${data.labelFacebook}  ${data.facebook}`;
 const portfolio = `${data.labelPortfolio}  ${data.portfolio}`;
 const bloging = `${data.labelBlog}  ${data.blog}`;
+const contact = `${data.labelContact}  ${data.contact}`;
 const carding = `${data.labelCard}  ${data.npx}`;
 
 const output = heading +
                newline + newline +
                working + newline + newline +
                twittering + newline +
+               facebooking + newline +
                npming + newline +
                pyping + newline +
                githubing + newline +
-               wantedlining + newline +
                portfolio + newline +
-               bloging + newline + newline +
+               bloging + newline +
+               contact + newline + newline +
                carding;
 
 writeFileSync(join(__dirname, "../bin/output"), chalk.green(boxen(output, options)));
