9abf7afc4b87a1a2b940389901141c473f694863
tubone <tubo.yyyuuu@gmail.com>
2025-04-09 10:27:30 +0900

    add mugi

--- src/build.ts
+++ src/build.ts
@@ -10,25 +10,40 @@ const options: Options = {
     borderStyle: BorderStyle.Round
 };
 
+const shibaInuArt = `_                          ,-､
+.:ヾ、            ,へ、__ /.  l
+.    l            |   /    ｀ヽ|
+>､__」       __ 人,/   tｯ  \`ｰ┐
+\`  ー―‐r'   :.        _ .. ┴ '′
+           ;     :.       \`ｰ-r┘
+.           ;      :.､__ ＿ _ﾉ
+         ;    ;.    └ー-rｨ
+         ',.   ｀'    ,..  -ﾉ
+/\`ｰ ､    }      ,:  __, /
+       \` -{    ,r‐i´   l
+           l    l   ',.   |
+            |   |    '   |
+            |.  l.    }  l_
+             ',  ヽ、\`:､_,.)
+             └-‐'`;
+
 const data = {
     name: chalk.white("               Yu Otsubo"),
     handle: chalk.white("tubone24"),
     work: chalk.white("FullCycle Developer") + chalk.cyan("@") + chalk.greenBright("KAG"),
     twitter: chalk.gray("https://twitter.com/") + chalk.cyan("meitante1conan"),
-    npm: chalk.gray("https://npmjs.com/") + chalk.red("~tubone24"),
-    pypi: chalk.gray("https://pypi.org/user/") + chalk.yellow("tubone24"),
     github: chalk.gray("https://github.com/") + chalk.green("tubone24"),
     facebook: chalk.gray("https://www.facebook.com/") + chalk.blueBright("yu.otsubo"),
     portfolio: chalk.cyan("https://portfolio.tubone-project24.xyz"),
     blog: chalk.cyan("https://blog.tubone-project24.xyz"),
     contact: chalk.cyan("https://portfolio.tubone-project24.xyz/#contact"),
+    instagram: chalk.gray("https://www.instagram.com/") + chalk.magenta("mugimugi.cutedog/"),
     npx: chalk.red("npx") + " " + chalk.white("tubone24"),
     labelWork: chalk.white.bold("       Work:"),
     labelTwitter: chalk.white.bold("    Twitter:"),
-    labelnpm: chalk.white.bold("        npm:"),
-    labelpypi: chalk.white.bold("       PyPI:"),
     labelGitHub: chalk.white.bold("     GitHub:"),
     labelFacebook: chalk.white.bold("   Facebook:"),
+    labelInstagram: chalk.white.bold("  Instagram:"),
     labelPortfolio: chalk.white.bold("  Portfolio:"),
     labelBlog: chalk.white.bold("       Blog:"),
     labelContact: chalk.white.bold("    Contact:"),
@@ -39,8 +54,7 @@ const newline = "\n";
 const heading = `${data.name} / ${data.handle}`;
 const working = `${data.labelWork}  ${data.work}`;
 const twittering = `${data.labelTwitter}  ${data.twitter}`;
-const npming = `${data.labelnpm}  ${data.npm}`;
-const pyping = `${data.labelpypi}  ${data.pypi}`;
+const instagraming = `${data.labelInstagram}  ${data.instagram}`;
 const githubing = `${data.labelGitHub}  ${data.github}`;
 const facebooking = `${data.labelFacebook}  ${data.facebook}`;
 const portfolio = `${data.labelPortfolio}  ${data.portfolio}`;
@@ -53,12 +67,14 @@ const output = heading +
                working + newline + newline +
                twittering + newline +
                facebooking + newline +
-               npming + newline +
-               pyping + newline +
+               instagraming + newline +
                githubing + newline +
                portfolio + newline +
                bloging + newline +
                contact + newline + newline +
                carding;
 
-writeFileSync(join(__dirname, "../bin/output"), chalk.green(boxen(output, options)));
+const cardOutput = chalk.green(boxen(output, options));
+const finalOutput = cardOutput + newline + chalk.yellow(shibaInuArt);
+
+writeFileSync(join(__dirname, "../bin/output"), finalOutput);
